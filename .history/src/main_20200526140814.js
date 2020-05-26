a12f417979b3a323952e30a798e7142443ee9817
ODell Sallis <odellsallis93@gmail.com>
2020-05-26 14:50:30 -0700

    First solid build

--- .history/src/main_20200526140814.js
+++ .history/src/main_20200526140814.js
@@ -1,7 +1,7 @@
-/* import { Application } from 'pixi.js'
+import { Application } from 'pixi.js'
 import {gsap} from "gsap"
 import {PixiPlugin} from "gsap/PixiPlugin.js"
-import {MotionPathPlugin} from "gsap/MotionPathPlugin.js" */
+import {MotionPathPlugin} from "gsap/MotionPathPlugin.js"
 
 //without this line, PixiPlugin and MotionPathPlugin may get dropped by your bundler (tree shaking)...
 // gsap.registerPlugin(PixiPlugin, MotionPathPlugin);
